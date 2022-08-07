896f998c68b4220d8cb0241d6ac43c825948919e
Alexsander Grichenok <alexsanderkorn90@gmail.com>
2022-08-08 00:23:03 +0300

    Helper hash, encryption

--- src/common/helper/helper.module.ts
+++ src/common/helper/helper.module.ts
@@ -1,7 +1,11 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { HelperArrayService } from './service/helper.array.service';
 import { HelperDateService } from './service/helper.date.service';
+import { HelperEncryptionService } from './service/helper.encryption.service';
 import { HelperGeoService } from './service/helper.geo.service';
+import { HelperHashService } from './service/helper.hash.service';
 import { HelperNumberService } from './service/helper.number.service';
 import { HelperService } from './service/helper.service';
 import { HelperStringService } from './service/helper.string.service';
@@ -15,7 +19,9 @@ import { HelperStringService } from './service/helper.string.service';
         HelperDateService,
         HelperArrayService,
         HelperGeoService,
-        HelperStringService
+        HelperStringService,
+        HelperHashService,
+        HelperEncryptionService
     ],
     exports: [
         HelperService,
@@ -23,8 +29,25 @@ import { HelperStringService } from './service/helper.string.service';
         HelperDateService,
         HelperArrayService,
         HelperGeoService,
-        HelperStringService
+        HelperStringService,
+        HelperHashService,
+        HelperEncryptionService
     ],
-    imports: [],
+    imports: [
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get<string>(
+                    'helper.jwt.defaultSecretKey'
+                ),
+                signOptions: {
+                    expiresIn: configService.get<string>(
+                        'helper.jwt.defaultExpirationTime'
+                    ),
+                },
+            }),
+        }),
+    ],
 })
 export class HelperModule {}
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import AppConfig from 'src/config/app.config';
 import DatabaseConfig from 'src/config/database.config';
+import HelperConfig from './helper.config';
 
 export default [
     AppConfig,
     DatabaseConfig,
+    HelperConfig
 ];
